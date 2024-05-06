c2507ba03341a14dc421b9f99c9ac89351921e3f
Purab Kashyap <purab2013@gmail.com>
2024-05-06 17:22:36 +0530

    Final build

--- app/src/main/java/com/example/quotes/MainActivity.kt
+++ app/src/main/java/com/example/quotes/MainActivity.kt
@@ -9,7 +9,10 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import com.example.quotes.databinding.ActivityMainBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,5 +26,46 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
+        getQuote()
+
+        binding.nextBtn.setOnClickListener {
+            getQuote()
+        }
+
+
+    }
+
+    private fun getQuote() {
+        setInProgress(true)
+
+        GlobalScope.launch {
+            try {
+                val response = RetrofitInstance.quoteApi.getRandomQuote()
+                runOnUiThread {
+                    setInProgress(false)
+                    response.body()?.first()?.let {
+                        setUI(it)
+                    }
+                }
+
+            }catch (e : Exception){
+
+            }
+        }
+
+    }
+
+    private fun setUI(quote : QuoteModel){
+        binding.quoteTv.text = quote.q
+        binding.authorTv.text = quote.a
+    }
+    private fun setInProgress(inProgress : Boolean){
+        if (inProgress){
+            binding.progressBar.visibility = View.VISIBLE
+            binding.nextBtn.visibility = View.GONE
+        } else {
+            binding.progressBar.visibility = View.GONE
+            binding.nextBtn.visibility = View.VISIBLE
+        }
     }
 }
